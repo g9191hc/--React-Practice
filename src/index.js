288012a138b077c211b5c8e64cf909b44846542e
현철 <g9191hc@gmail.com>
2022-11-23 00:55:00 +0900

    start

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import Clock from './chapter_04/Clock';
+
+const root = ReactDOM.createRoot(document.getElementById('root')); //VirtualDOM의 최상위노드 root 생성
+
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <Clock />
+    </React.StrictMode>
+  );
+}, 1000
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
